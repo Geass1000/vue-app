db64f304fbb76e502d3e31fba9d745ff703a5039
Andrey <geass1000@gmail.com>
2019-06-05 13:56:17 +0300

    change: inject and use LoggerService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,27 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 import { AppRouter } from './app.router';
 
-import { HttpService } from './core/services/http.service';
+import { HttpService, LoggerService } from './core/services';
 
 @Component({
-    diInject: { http: HttpService },
+    diProvide: [ LoggerService ],
+    diInject: { http: HttpService, logger: LoggerService },
     router: AppRouter,
 })
 export default class AppComponent extends Vue {
     public showHello: boolean = true;
 
     public http!: HttpService;
+    public logger!: LoggerService;
+
+    public created () {
+        this.logger.className = `AppComponent`;
+    }
 
     public greet () {
         this.showHello = !this.showHello;
-        this.http.get();
+        this.logger.info(`greet`, `User clicked on the 'Data' button`);
+        // this.http.get();
     }
 
     public newData () {
--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,22 +1,26 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-import { HttpService } from '../core/services/http.service';
+import { HttpService, LoggerService } from '../core/services';
 
 @Component({
     diProvide: [
         HttpService,
+        LoggerService,
     ],
     diInject: {
         http: HttpService,
+        logger: LoggerService,
     }
 })
 export default class HelloWorldComponent extends Vue {
     @Prop() private msg!: string;
 
     public http!: HttpService;
+    public logger!: LoggerService;
 
     public created () {
         this.http.post();
-        console.log(`Element was ceated with message ${this.msg}`);
+        this.logger.className = `HelloWorldComponent`;
+        this.logger.log(`created`, `Element was ceated with message ${this.msg}`);
     }
 }
