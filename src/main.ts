517e00b34c3b239a937a25df87bfebb668c41ff3
Andrey <geass1000@gmail.com>
2019-06-05 10:31:57 +0300

    change: register the DI Container and global services

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import Vue from 'vue';
 import AppComponent from './app/app.component.vue';
 
+import { VueDiContainer } from 'vue-di-container';
+
+import { serviceProvider } from './app/core/global.provider';
+
 Vue.config.productionTip = false;
 
+Vue.use(VueDiContainer);
+
 new Vue({
-  render: (h) => h(AppComponent),
+    diProvide: [
+        ...serviceProvider,
+    ],
+    render: (h) => h(AppComponent),
 }).$mount('#app');
