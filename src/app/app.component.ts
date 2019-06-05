01585907b3df304d191ced21c48f788b32bc2074
Andrey <geass1000@gmail.com>
2019-06-05 10:33:32 +0300

    add: test logic for the DI Container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,22 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 import { AppRouter } from './app.router';
 
+import { HttpService } from './core/services/http.service';
+
 @Component({
+    diInject: { http: HttpService },
     router: AppRouter,
 })
 export default class AppComponent extends Vue {
     public showHello: boolean = true;
 
+    public http!: HttpService;
+
     public greet () {
         this.showHello = !this.showHello;
+        this.http.get();
     }
 
-    // Реагирует на изменение данных, но не кэширует результат
     public newData () {
         return this.showHello ? `Data 1` : `Data 2`;
     }
--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,10 +1,22 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-@Component({})
+import { HttpService } from '../core/services/http.service';
+
+@Component({
+    diProvide: [
+        HttpService,
+    ],
+    diInject: {
+        http: HttpService,
+    }
+})
 export default class HelloWorldComponent extends Vue {
     @Prop() private msg!: string;
 
+    public http!: HttpService;
+
     public created () {
+        this.http.post();
         console.log(`Element was ceated with message ${this.msg}`);
     }
 }
