db137c52811b0131ea443280646ede5f428f452f
Andrey <geass1000@gmail.com>
2019-06-06 12:52:04 +0300

    add: using of the `Vuetify` package in vue engine

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import 'reflect-metadata';
 import Vue from 'vue';
 import AppComponent from './app/app.component.vue';
 
+import 'vuetify/dist/vuetify.min.css';
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+
 Vue.config.productionTip = false;
 
 new Vue({
