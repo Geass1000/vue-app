3be2b7fd6558666327bcb50363c22de1a928690f
Andrey <geass1000@gmail.com>
2019-06-07 15:09:28 +0300

    add: example of using vuex store in components

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 import ProfileNavigationComponent from './navigation/navigation.component.vue';
 
 import { HttpService, LoggerService } from '../core/services';
+import { StoreKey, Store, StoreState, AppEvent } from '../vuex';
 
 import { ProfileModule } from './profile.module';
 
@@ -18,9 +19,20 @@ export default class ProfileComponent extends Vue {
     @ProfileModule.lazyInject(HttpService.diIdentifier)
     private http!: HttpService;
 
+    @ProfileModule.lazyInject(StoreKey)
+    private store!: Store<StoreState>;
+
     public created () {
         this.http.post();
+
+        this.store.commit(`${AppEvent.mutations.ToggleDrawer}`);
+        setTimeout(() => this.store.commit(`${AppEvent.mutations.ToggleDrawer}`), 4000);
+
         this.logger.className = `HelloWorldComponent`;
         this.logger.log(`created`, `Element was ceated with message`);
+    }
+
+    get drawerState (): boolean {
+        return this.store.state.app.drawerState;
     }
 }
