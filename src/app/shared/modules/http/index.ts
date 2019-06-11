79a8e60a960a59b8f6a1c26d06a60e6aac107a8c
Andrey <geass1000@gmail.com>
2019-06-11 18:31:58 +0300

    add: `index` reexport file

---
+++ src/app/shared/modules/http/index.ts
@@ -0,0 +1,10 @@
+export * from './http.request';
+export * from './http.response';
+export * from './http.service';
+export * from './http.meta';
+
+import * as interfaces from './http.interface';
+import * as enums from './http.enum';
+
+export const Interfaces = interfaces;
+export const Enums = enums;
